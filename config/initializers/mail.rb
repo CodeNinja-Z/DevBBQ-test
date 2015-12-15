18bb1a4cd7e890d6e96614078138d562b18d1255
Carlos Zhao <blacklight728@gmail.com>
2015-12-14 21:08:05 -0500

    Change SendGrid setting from development to heroku(productionable).

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,28 +1,29 @@
 ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
 
+# For development:
+# ActionMailer::Base.smtp_settings = {
+#   :address => 'smtp.sendgrid.net',
+#   :port => '2525',
+#   :domain => 'example.com',
+#   :authentication => :plain,
+#   :user_name => ENV['SENDGRID_USERNAME'],
+#   :password => ENV['SENDGRID_PASSWORD']
+# }
+
 # For using Heroku:
-# if ENV['SENDGRID_USERNAME'] && ENV['SENDGRID_PASSWORD']
-#   ActionMailer::Base.smtp_settings = {
-#     :address        => 'smtp.sendgrid.net',
-#     :port           => '465',
-#     :authentication => :plain,
-#     :user_name      => ENV['SENDGRID_USERNAME'],
-#     :password       => ENV['SENDGRID_PASSWORD'],
-#     :domain         => 'heroku.com',
-#     :enable_starttls_auto => true,
-#     :ssl => true
-#   }
-#   ActionMailer::Base.delivery_method = :smtp
-# else # For development
+if ENV['SENDGRID_USERNAME'] && ENV['SENDGRID_PASSWORD']
   ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '2525',
-  :domain => 'example.com',
-  :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD']
-}
-# end
+    :address        => 'smtp.sendgrid.net',
+    :port           => '465',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true,
+    :ssl => true
+  }
+  ActionMailer::Base.delivery_method = :smtp
+end
 
 SendGrid.configure do |config|
   config.dummy_recipient = 'blacklight728@gmail.com'
