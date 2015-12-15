202f1392c4e6cb09832177dba7e87b19ea3b21a6
Carlos Zhao <blacklight728@gmail.com>
2015-12-15 16:29:54 -0500

    Finish message sending functionality

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use SCSS for stylesheets
+gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
@@ -29,6 +30,10 @@ gem 'sendgrid-rails'
 
 gem "figaro"
 
+gem "chartkick"
+
+gem 'groupdate'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -39,6 +44,7 @@ gem "figaro"
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
+  gem 'pry-rails'
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,29 +2,26 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    client = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "DevBBQ_development")
-    # Q: How to avoid SQL injection on the next line?
-    @messages = client.query("SELECT * FROM messages WHERE user_id = #{current_user.id}")
-    # @message = current_user.messages.build
-    @message = Message.new
-    respond_to do |format|
-      format.html
-      format.js
-    end
+    # client = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "DevBBQ_development")
+    # @messages = client.query("SELECT * FROM messages WHERE user_id = #{current_user.id}")
+    # @messages = ActiveRecord::Base.connection.select_all("SELECT * FROM messages WHERE user_id = #{current_user.id}").rows
+    @messages = Message.where(user_id: current_user.id)
+    @message = current_user.messages.build
   end
 
   def create
-    subject = params[:title]
-    body = params[:content]
     @message = Message.new(message_params)
-    if @message.save!
-      MessageMailer.devbbq_email(subject, body).deliver_now
-      redirect_to root_path
+    respond_to do |format|
+      if @message.save!
+        MessageMailer.devbbq_email(@message.title, @message.content).deliver_now
+      end
+      format.html
+      format.js
     end   
   end
 
   private
   def message_params
-    params.require(:message).permit(:title, :content)
+    params.require(:message).permit(:title, :content, :user_id)
   end
 end
