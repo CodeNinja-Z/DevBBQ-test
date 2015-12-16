3ff79a4f23bd5a7d0b35b645844521fe8e47f947
Carlos Zhao <blacklight728@gmail.com>
2015-12-15 23:21:09 -0500

    Add admin account under development mode.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212222943) do
+ActiveRecord::Schema.define(version: 20151216040831) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20151212222943) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
